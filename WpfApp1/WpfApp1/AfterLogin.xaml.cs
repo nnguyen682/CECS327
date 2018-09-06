17a0c8a2beee3d84b66d77d1c180c8452347e413
nnguyen682 <35628819+nnguyen682@users.noreply.github.com>
2018-09-06 11:13:56 -0700

    fixed bugs

--- WpfApp1/WpfApp1/AfterLogin.xaml.cs
+++ WpfApp1/WpfApp1/AfterLogin.xaml.cs
@@ -24,15 +24,15 @@ namespace WpfApp1
     public partial class AfterLogin : Window
     {
 
-        public static List<string> mediaFileList ;
+        public static List<string> mediaFileList;
         public static User objectUser;
-        public static Window afterLoginWindow ;
+        public static Window afterLoginWindow;
         public static AxWMPLib.AxWindowsMediaPlayer ax;
         string mediaFolder = System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, "MusicLibrary");
         public AfterLogin()
         {
             InitializeComponent();
-            
+
             DirectoryInfo dir = new DirectoryInfo(mediaFolder);
             foreach (FileInfo file in dir.GetFiles("*.*", SearchOption.AllDirectories))
             {
@@ -45,7 +45,7 @@ namespace WpfApp1
                 list.ItemsSource = mediaFileList;
             }
         }
-        
+
         public AfterLogin(User x)
         {
             objectUser = new User();
@@ -55,10 +55,10 @@ namespace WpfApp1
             ax =
                 winsFormHost.Child as AxWMPLib.AxWindowsMediaPlayer;
             objectUser = x;
-            profile.Source = new BitmapImage(new Uri("pack://application:,,,/Images/"+ x.mUsername + ".png"));
-            
+            profile.Source = new BitmapImage(new Uri("pack://application:,,,/Images/" + x.mUsername + ".png"));
+
             DirectoryInfo dir = new DirectoryInfo(mediaFolder);
-            
+
             foreach (FileInfo file in dir.GetFiles("*.*", SearchOption.AllDirectories))
             {
                 if (file.Extension == ".mp3" || file.Extension == ".mp4")
@@ -70,16 +70,17 @@ namespace WpfApp1
                 list.ItemsSource = mediaFileList;
                 ax.URL = mediaFolder + "\\" + mediaFileList[0];
             }
-            
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
-                private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ax.URL = mediaFolder+ "\\" + list.SelectedItem.ToString();
+            if (list.SelectedItem != null)
+                ax.URL = mediaFolder + "\\" + list.SelectedItem.ToString();
         }
 
         private void Button_Profile(object sender, RoutedEventArgs e)
@@ -111,6 +112,10 @@ namespace WpfApp1
             Create b = new Create();
             this.Hide();
             b.Show();
+        }
+        protected virtual void OnFormClosing(System.Windows.Forms.FormClosingEventArgs e)
+        {
+
         }
     }
 }
